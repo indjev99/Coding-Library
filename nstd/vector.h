05a376633a3ab47e68e5f2aa02a4d6fa45a1dc76
Emil Indzhev <emil.indjev@gmail.com>
2020-01-12 00:18:51 +0200

    Update vector.h
    Should be done. Has been tested for correctness and memory leaks.
    
    Doesn't use allocators, but could easily be adapted to do so.

--- nstd/vector.h
+++ nstd/vector.h
@@ -1,11 +1,10 @@
 #pragma once
 
 #include <iterator>
-#include <algorithm>
+#include <exception>
+#include "algorithm.h"
 
 namespace nstd { template <typename T> class vector; }
-
-/// TO-DO: Fix to use allocators
 
 template <typename T>
 class nstd::vector
@@ -18,212 +17,289 @@ public:
     typedef const value_type&                     const_reference;
     typedef value_type*                           pointer;
     typedef const value_type*                     const_pointer;
+    typedef size_t                                size_type;
+    typedef ptrdiff_t                             difference_type;
     typedef value_type*                           iterator;
     typedef const value_type*                     const_iterator;
     typedef std::reverse_iterator<iterator>       reverse_iterator;
     typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-    typedef size_t                                size_type;
-    typedef ptrdiff_t                             difference_type;
 
     // Constructors
-    vector()                              noexcept : size_(0), capacity_(0), data_(nullptr) {}
-    vector(size_type cnt)                          : size_(cnt), capacity_(cnt), data_(new value_type[capacity_]) {}
-    vector(size_type cnt, const_reference val)     : vector(cnt)         { std::fill(data_, data_ + size_, val); }
-    vector(const vector& other)                    : vector(other.size_) { std::copy(other.data_, other.data_ + size_, data_); }
-    vector(vector&& other)                noexcept : size_(other.size_), capacity_(other.capacity_), data_(other.data_) { other.steal_contents(); }
-    vector(std::initializer_list<value_type> vals) : vector(vals.size()) { std::copy(vals.begin(), vals.end(), data_); }
-    template <class InputIter, typename = std::_RequireInputIter<InputIter>>
-    vector(InputIter first, InputIter last)        : vector()            { std::copy(first, last, std::back_insert_iterator<vector>(*this)); }
+    vector() noexcept : size_(0), capacity_(0), data_(nullptr) {}
+    vector(size_type cnt) : size_(cnt), capacity_(size_)
+    {
+        data_ = allocate_data(capacity_);
+        nstd::construct(begin(), end());
+    }
+    vector(size_type cnt, const_reference val) : size_(cnt), capacity_(size_)
+    {
+        data_ = allocate_data(capacity_);
+        nstd::construct_fill(begin(), end(), val);
+    }
+    vector(const vector& other) : size_(other.size()), capacity_(size_)
+    {
+        data_ = allocate_data(capacity_);
+        nstd::construct_copy(other.begin(), other.end(), begin());
+    }
+    vector(vector&& other) noexcept : size_(other.size_), capacity_(other.capacity_), data_(other.data_)
+    {
+        other.steal_contents();
+    }
+    vector(std::initializer_list<value_type> vals) : size_(vals.size()), capacity_(size_)
+    {
+        data_ = allocate_data(capacity_);
+        nstd::construct_copy(vals.begin(), vals.end(), begin());
+    }
+    template <typename InputIter, typename = std::_RequireInputIter<InputIter>>
+    vector(InputIter first, InputIter last) : vector()
+    {
+        nstd::copy(first, last, std::back_insert_iterator<vector>(*this));
+    }
 
     // Destructor
-    ~vector() { delete[] data_; }
+    ~vector()
+    {
+        destroy_data();
+    }
 
-    // Operator =
+    // Assignment
     vector& operator=(const vector& other)
     {
         if (this == &other) return *this;
-        allocate_new_data(other.size_);
-        std::copy(other.data_, other.data_ + size_, data_);
+        new_data(other.size());
+        nstd::construct_copy(other.begin(), other.end(), begin());
         return *this;
     }
     vector& operator=(vector&& other) noexcept
     {
         if (this == &other) return *this;
+        destroy_data();
+        data_ = other.data_;
         size_ = other.size_;
         capacity_ = other.capacity_;
-        delete[] data_;
-        data_ = other.data_;
         other.steal_contents();
         return *this;
     }
-
-    // Assign
-    void assign(size_type cnt, const value_type& val)   { allocate_new_data(cnt); std::fill(data_, data_ + size_, val);               }
-    void assign(std::initializer_list<value_type> vals) { allocate_new_data(vals.size()); std::copy(vals.begin(), vals.end(), data_); }
-    template <class InputIter, typename = std::_RequireInputIter<InputIter>>
-    void assign(InputIter first, InputIter last)        { clear(); std::copy(first, last, std::back_insert_iterator<vector>(*this));  }
+    void assign(size_type cnt, const_reference val)
+    {
+        new_data(cnt);
+        nstd::construct_fill_n(begin(), cnt, val);
+    }
+    void assign(std::initializer_list<value_type> vals)
+    {
+        new_data(vals.size());
+        nstd::construct_copy(vals.begin(), vals.end(), begin());
+    }
+    template <typename InputIter, typename = std::_RequireInputIter<InputIter>>
+    void assign(InputIter first, InputIter last)
+    {
+        clear();
+        nstd::copy(first, last, std::back_insert_iterator<vector>(*this));
+    }
 
     // Iterators
-    iterator begin()                      noexcept { return data_;         }
-    const_iterator begin()          const noexcept { return data_;         }
-    const_iterator cbegin()         const noexcept { return data_;         }
-    iterator end()                        noexcept { return data_ + size_; }
-    const_iterator end()            const noexcept { return data_ + size_; }
-    const_iterator cend()           const noexcept { return data_ + size_; }
-    reverse_iterator rbegin()             noexcept { return reverse_iterator(data_ + size_); }
-    const_reverse_iterator rbegin() const noexcept { return reverse_iterator(data_ + size_); }
-    const_reverse_iterator crbegin()const noexcept { return reverse_iterator(data_ + size_); }
-    reverse_iterator rend()               noexcept { return reverse_iterator(data_);         }
-    const_reverse_iterator rend()   const noexcept { return reverse_iterator(data_);         }
-    const_reverse_iterator crend()  const noexcept { return reverse_iterator(data_);         }
+    iterator begin()                       noexcept { return data_;         }
+    const_iterator begin()           const noexcept { return data_;         }
+    const_iterator cbegin()          const noexcept { return data_;         }
+    iterator end()                         noexcept { return data_ + size_; }
+    const_iterator end()             const noexcept { return data_ + size_; }
+    const_iterator cend()            const noexcept { return data_ + size_; }
+    reverse_iterator rbegin()              noexcept { return reverse_iterator(data_ + size_); }
+    const_reverse_iterator rbegin()  const noexcept { return reverse_iterator(data_ + size_); }
+    const_reverse_iterator crbegin() const noexcept { return reverse_iterator(data_ + size_); }
+    reverse_iterator rend()                noexcept { return reverse_iterator(data_);         }
+    const_reverse_iterator rend()    const noexcept { return reverse_iterator(data_);         }
+    const_reverse_iterator crend()   const noexcept { return reverse_iterator(data_);         }
 
-    // Capacity
-    bool empty()                    const noexcept { return size_ == 0; }
-    size_type size()                const noexcept { return size_;      }
-    size_type max_size()            const noexcept { return SIZE_MAX;   }
-    size_type capacity()            const noexcept { return capacity_;  }
-    void reserve(size_type cnt)                    { if (capacity_ < cnt) change_capacity(cnt);    }
-    void shrink_to_fit()                           { if (capacity > size_) change_capacity(size_); }
+    // Size
+    bool empty()                     const noexcept { return size_ == 0; }
+    size_type size()                 const noexcept { return size_;      }
+    size_type max_size()             const noexcept { return SIZE_MAX;   }
+    size_type capacity()             const noexcept { return capacity_;  }
+    void reserve(size_type cnt)                     { if (capacity_ < cnt) change_capacity(cnt);    }
+    void shrink_to_fit()                            { if (capacity > size_) change_capacity(size_); }
 
     // Element access
-    reference operator[](size_type idx)            { return data_[idx];       }
-    const_reference operator[](size_type idx)const { return data_[idx];       }
-    reference front()                              { return data_[0];         }
-    const_reference front()                  const { return data_[0];         }
-    reference back()                               { return data_[size_ - 1]; }
-    const_reference back()                   const { return data_[size_ - 1]; }
-    pointer data()                        noexcept { return data_;            }
-    const_pointer data()            const noexcept { return data_;            }
-    reference at(size_type idx)                    { if (idx >= size_) throw std::out_of_range(); return data_[idx]; }
-    const_reference at(size_type idx)        const { if (idx >= size_) throw std::out_of_range(); return data_[idx]; }
+    reference operator[](size_type idx)             { return data_[idx];       }
+    const_reference operator[](size_type idx) const { return data_[idx];       }
+    reference front()                               { return data_[0];         }
+    const_reference front()                   const { return data_[0];         }
+    reference back()                                { return data_[size_ - 1]; }
+    const_reference back()                    const { return data_[size_ - 1]; }
+    pointer data()                         noexcept { return data_;            }
+    const_pointer data()             const noexcept { return data_;            }
+    reference at(size_type idx)                     { if (idx >= size_) throw std::out_of_range(); return data_[idx]; }
+    const_reference at(size_type idx)         const { if (idx >= size_) throw std::out_of_range(); return data_[idx]; }
 
     // Modifiers
-    void clear()                          noexcept { size_ = 0;                      }
-    void push_back(const value_type& val)          { expand(); data_[size_++] = val; }
-    void push_back(value_type&& val)               { expand(); data_[size_++] = val; }
-    void pop_back()                                { --size_;                        }
-    void resize(size_type cnt)                     { reserve(cnt); size_ = cnt;      }
-    iterator insert(const_iterator pos, const value_type& val)
+    void clear()                           noexcept { shrink_resize(0);             }
+    void push_back(const_reference val)             { emplace_back(val);            }
+    void push_back(value_type&& val)                { emplace_back(std::move(val)); }
+    void pop_back()                                 { data_[--size_].~value_type(); }
+
+    iterator insert(const_iterator pos, const_reference val) { return emplace(pos, val);            }
+    iterator insert(const_iterator pos, value_type&& val)    { return emplace(pos, std::move(val)); }
+    iterator insert(const_iterator pos, size_type cnt, const_reference val)
     {
-        iterator new_pos = shift_right(pos - data_, 1);
-        *new_pos = val;
-        return new_pos;
-    }
-    iterator insert(const_iterator pos, value_type&& val)
-    {
-        iterator new_pos = shift_right(pos - data_, 1);
-        *new_pos = val;
-        return new_pos;
-    }
-    iterator insert(const_iterator pos, size_type cnt, const value_type& val)
-    {
-        iterator new_pos = shift_right(pos - data_, cnt);
-        std::fill(new_pos, new_pos + cnt, val);
+        difference_type offset = pos - begin();
+        iterator mid = shift_right(offset, cnt);
+        iterator new_pos = iter_at(offset);
+        nstd::fill(new_pos, mid, val);
+        nstd::construct_fill(mid, new_pos + cnt, val);
         return new_pos;
     }
     iterator insert(const_iterator pos, std::initializer_list<value_type> vals)
     {
-        iterator new_pos = shift_right(pos - data_, vals.size());
-        std::copy(vals.begin(), vals.end(), new_pos);
+        difference_type offset = pos - begin();
+        iterator mid = shift_right(offset, vals.size());
+        iterator new_pos = iter_at(offset);
+        auto curr = nstd::copy_to(vals.begin(), new_pos, mid);
+        nstd::construct_copy(curr, vals.end(), mid);
         return new_pos;
     }
-    template <class InputIter, typename = std::_RequireInputIter<InputIter>>
+    template <typename InputIter, typename = std::_RequireInputIter<InputIter>>
     iterator insert(const_iterator pos, InputIter first, InputIter last)
     {
-        difference_type offset = pos - data_;
-        std::copy(first, last, std::insert_iterator<vector>(*this, data_ + offset));
-        return data_ + offset;
+        difference_type offset = pos - begin();
+        nstd::copy(first, last, std::insert_iterator<vector>(*this, iter_at(offset)));
+        return iter_at(offset);
     }
-    template <class... Args>
+    template <typename... Args>
     iterator emplace(const_iterator pos, Args&&... args)
     {
-        iterator new_pos = shift_right(pos - data_, 1);
-        reference obj = *new_pos;
-        obj.~value_type();
-        new(&obj) value_type(args...);
+        difference_type offset = pos - begin();
+        iterator mid = shift_right(offset, 1);
+        iterator new_pos = iter_at(offset);
+        iterator left = new_pos;
+        if (left != mid)
+            *left = std::move(value_type(std::forward<Args>(args)...));
+        else
+            new(left) value_type(std::forward<Args>(args)...);
         return new_pos;
     }
-    template <class... Args>
+    template <typename... Args>
     reference emplace_back(Args&&... args)
     {
-        expand();
+        expand(size_ + 1);
         reference obj = data_[size_++];
-        obj.~value_type();
-        new(&obj) value_type(args...);
+        new(&obj) value_type(std::forward<Args>(args)...);
         return obj;
     }
     iterator erase(const_iterator pos)
     {
-        shift_left(pos + 1 - data_, 1);
-        return data_ + (pos - data_);
+        difference_type offset = pos - begin();
+        shift_left(pos + 1 - begin(), 1);
+        return iter_at(offset);
     }
     iterator erase(const_iterator first, const_iterator last)
     {
-        shift_left(last - data_, last - first);
-        return data_ + (first - data_);
+        difference_type offset = first - begin();
+        shift_left(last - begin(), last - first);
+        return begin() + offset;
+    }
+    void resize(size_type cnt)
+    {
+        if (cnt < size_) shrink_resize(cnt);
+        else
+        {
+            expand(cnt);
+            nstd::construct(end(), iter_at(cnt));
+            size_ = cnt;
+        }
     }
     void resize(size_type cnt, const_reference val)
     {
-        size_t old_size = size_;
-        resize(cnt);
-        if (old_size < size_) std::fill(data_ + old_size, data_ + size_, val);
+        if (cnt < size_) shrink_resize(cnt);
+        else
+        {
+            expand(cnt);
+            nstd::construct_fill(end(), iter_at(cnt), val);
+            size_ = cnt;
+        }
     }
     void swap(vector& other) noexcept
     {
+        std::swap(data_, other.data_);
         std::swap(size_, other.size_);
         std::swap(capacity_, other.capacity_);
-        std::swap(data_, other.data_);
     }
 
 private:
 
+    iterator iter_at(size_type idx)
+    {
+        return data_ + idx;
+    }
+    void shrink_resize(size_type cnt)
+    {
+        nstd::destruct(iter_at(cnt), end());
+        size_ = cnt;
+    }
+    size_type expand_size(size_type cnt)
+    {
+        return nstd::max(capacity_ * 2, cnt);
+    }
+    void expand(size_type cnt)
+    {
+        if (capacity_ < cnt) change_capacity(expand_size(cnt));
+    }
     void shift_left(difference_type from, difference_type dist)
     {
-        std::move(data_ + from, data_ + size_, data_ + from - dist);
-        size_ -= dist;
+        nstd::move(iter_at(from), end(), iter_at(from - dist));
+        shrink_resize(size_ - dist);
     }
     iterator shift_right(difference_type from, difference_type dist)
     {
         size_type new_size = size_ + dist;
-        iterator from_it = data_ + from;
         if (capacity_ < new_size)
         {
-            capacity_ *= 2;
-            if (capacity_ < new_size) capacity_ = new_size;
-            value_type* new_data = new value_type[capacity_];
-            std::move(data_, from_it, new_data);
-            std::move(from_it, data_ + size_, new_data + from + dist);
-            delete[] data_;
+            size_type new_capacity = expand_size(new_size);
+            value_type* new_data = allocate_data(new_capacity);
+            nstd::construct_move(begin(), iter_at(from), new_data);
+            nstd::construct_move(iter_at(from), end(), new_data + from + dist);
+            destroy_data();
             data_ = new_data;
+            size_ = new_size;
+            capacity_ = new_capacity;
+            return iter_at(from);
         }
         else
         {
-            std::move_backward(from_it, data_ + size_, data_ + size_ + dist);
+            size_type num = nstd::min(size_ - from, size_type(dist));
+            iterator mid = nstd::construct_move_backward_n(end(), num, iter_at(new_size));
+            nstd::move_backward(iter_at(from), iter_at(size_ - num), mid);
+            iterator ret = nstd::min(end(), iter_at(from + dist));
+            size_ = new_size;
+            return ret;
         }
-        size_ = new_size;
-        return data_ + from;
-    }
-    void expand()
-    {
-        if (size_ == capacity_) change_capacity(capacity_ ? capacity_ * 2 : 1);
     }
     void change_capacity(size_type cnt)
     {
-        capacity_ = cnt;
-        value_type* new_data = new value_type[capacity_];
-        std::move(data_, data_ + size_, new_data);
-        delete[] data_;
+        value_type* new_data = allocate_data(cnt);
+        nstd::construct_move(data_, data_ + size_, new_data);
+        destroy_data();
         data_ = new_data;
+        capacity_ = cnt;
     }
-    void allocate_new_data(size_type cnt)
+    void new_data(size_type cnt)
     {
+        clear();
+        reserve(cnt);
         size_ = cnt;
-        if (capacity_ < size_)
-        {
-            capacity_ = size_;
-            delete[] data_;
-            data_ = new value_type[capacity_];
-        }
+    }
+    void destroy_data()
+    {
+        nstd::destruct(begin(), end());
+        deallocate_data(data_, capacity_);
+    }
+
+    static value_type* allocate_data(size_type cnt)
+    {
+        return static_cast<value_type*>(malloc(cnt * sizeof(value_type)));
+    }
+    static void deallocate_data(value_type* data, size_type cnt)
+    {
+        free(data);
     }
 
     void steal_contents() { size_ = 0; capacity_ = 0; data_ = nullptr; }
